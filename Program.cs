1fa1c5569a41552148fdc0ed31140e789f5c46bd
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-05-25 23:59:04 +0200

    Edited Program.cs by making a lambda function demo comparison

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async ctx =>
+{
+    await ctx.Response.WriteAsync("Welcome to FreeBillng");
+});
+
+//app.Run(async ctxx =>
+//{
+//    await ctxx.Response.WriteAsync("<html>" +
+//                                        "<body>" +
+//                                            "<h1>Welcome to FreeBilling</h1>" +
+//                                        "</body>" +
+//                                  "</html>");
+//});
 
 app.Run();
