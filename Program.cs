f1bb45ffaae1b4965d5c97d8c5fc0d019a4ab835
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-05-30 23:36:26 +0200

    Added a razor page middleware for rendering it

--- Program.cs
+++ Program.cs
@@ -1,23 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseDeveloperExceptionPage();
 
 app.UseDefaultFiles();
 
 app.UseStaticFiles();
 
-//app.Run(async ctx =>
-//{
-//    await ctx.Response.WriteAsync("Welcome to FreeBillng");
-//});
-
-//app.Run(async ctxx =>
-//{
-//    await ctxx.Response.WriteAsync("<html>" +
-//                                        "<body>" +
-//                                            "<h1>Welcome to FreeBilling</h1>" +
-//                                        "</body>" +
-//                                  "</html>");
-//});
+app.MapRazorPages();
 
 app.Run();
