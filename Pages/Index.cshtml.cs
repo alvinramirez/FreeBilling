34cb8180dd83f04c6dbd554ea5f684b37d4d328a
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-05-30 23:37:25 +0200

    Created a new directory for render the razor pages

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FreeBilling.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
