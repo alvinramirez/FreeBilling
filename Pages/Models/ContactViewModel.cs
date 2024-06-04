8e428842bf53ff1687ae267fb347e26f68da78e0
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-04 23:29:13 +0200

    Added attribute of required using DataAnnotations namespace

--- Pages/Models/ContactViewModel.cs
+++ Pages/Models/ContactViewModel.cs
@@ -1,10 +1,17 @@
-﻿namespace FreeBilling.Web.Pages.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeBilling.Web.Pages.Models
 {
     public class ContactViewModel
     {
+        [Required]
         public string Name { get; set; } = "";
+        [EmailAddress]
         public string Email { get; set; } = "";
+        [Required]
         public string Subject { get; set; } = "";
+        [Required]
+        [MinLength(25)]
         public string Body { get; set; } = "";
     }
 }
