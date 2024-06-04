30720081a0065acfabd735c65f8e22bd478554a0
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-04 23:02:21 +0200

    Created a new directory for a ContactView model binding accepting the data we want

---
+++ Pages/Models/ContactViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeBilling.Web.Pages.Models
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Subject { get; set; } = "";
+        public string Body { get; set; } = "";
+    }
+}
