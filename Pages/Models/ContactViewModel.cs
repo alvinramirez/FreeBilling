235f1fc55307752c816d8becd63dfea406f59e51
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-05 22:57:30 +0200

    Added validation decorates for each property for this view model

--- Pages/Models/ContactViewModel.cs
+++ Pages/Models/ContactViewModel.cs
@@ -6,10 +6,14 @@ namespace FreeBilling.Web.Pages.Models
     {
         [Required]
         public string Name { get; set; } = "";
+
+        [Required]
         [EmailAddress]
         public string Email { get; set; } = "";
+        
         [Required]
         public string Subject { get; set; } = "";
+
         [Required]
         [MinLength(25)]
         public string Body { get; set; } = "";
