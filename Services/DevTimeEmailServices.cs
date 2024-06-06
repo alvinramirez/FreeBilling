63bf0b8cd9da84afd451681ba030b6f0c3306507
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-06 16:37:39 +0200

    Created a new directory to create a service for send the email using an interface and a fake email service

---
+++ Services/DevTimeEmailServices.cs
@@ -0,0 +1,16 @@
+﻿namespace FreeBilling.Web.Services
+{
+    public class DevTimeEmailServices : IEmailService
+    {
+        private readonly ILogger<DevTimeEmailServices> logger;
+
+        public DevTimeEmailServices(ILogger<DevTimeEmailServices> logger)
+        {
+            this.logger = logger;
+        }
+        public void SendMail(string to, string from, string subject, string body)
+        {
+            this.logger.LogInformation($"Sending Email to {to} with a subject of {subject}");
+        }
+    }
+}
---
+++ Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeBilling.Web.Services
+{
+    public interface IEmailService
+    {
+        void SendMail(string to, string from, string subject, string body);
+    }
+}
