1f75d39a2f6770a41dc47a857f6c6ca5825d3740
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-09 23:20:46 +0200

    Created a setup configuration tailwind file telling it were our data is

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Pages/**/*.{html,cshtml}",
+        "./wwwroot/**/*.{html,htm}"
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
