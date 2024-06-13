9d031fb5b7ef9a23377c6280298b6d89c67803d6
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-13 23:47:16 +0200

    Added a override method for using SqlServer as the Database engine

--- Data/BillingContext.cs
+++ Data/BillingContext.cs
@@ -8,5 +8,13 @@ namespace FreeBilling.Web.Data
         public DbSet<Customer> Customers => Set<Customer>();
         public DbSet<Employee> Employees => Set<Employee>();
         public DbSet<TimeBill> TimeBills => Set<TimeBill>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var connectionString = "";
+            optionsBuilder.UseSqlServer();
+        }
     }
 }
