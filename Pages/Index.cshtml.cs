600143e96d8577f911043d4ed5ebae0cbe94b106
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-16 21:07:50 +0200

    Created a async method for load a customers list from the database

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,25 @@
+using FreeBilling.Data.Entities;
+using FreeBilling.Web.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FreeBilling.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly BillingContext context;
+
+        public IndexModel(BillingContext context)
         {
+            this.context = context;
+        }
+
+        public List<Customer>? Customers { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Customers = await this.context.Customers.ToListAsync();
         }
     }
 }
