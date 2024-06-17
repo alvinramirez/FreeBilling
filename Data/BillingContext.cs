09628e8f996964f2520809e1486cdaa83e523943
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-17 21:58:02 +0200

    Added some data inserted into the database showing the customers on the main page

--- Data/BillingContext.cs
+++ Data/BillingContext.cs
@@ -22,5 +22,81 @@ namespace FreeBilling.Web.Data
             var connectionString = _config["ConnectionStrings:BillingDb"];
             optionsBuilder.UseSqlServer(connectionString);
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Address>()
+                .HasData(
+                    new Address()
+                    {
+                        Id = 1,
+                        AddressLine1 = "123 Main Street",
+                        City = "Atlanta",
+                        StateProvince = "GA",
+                        PostalCode = "12345"
+                    },
+                    new Address()
+                    {
+                        Id = 2,
+                        AddressLine1 = "123 First Avenue",
+                        City = "Atlanta",
+                        StateProvince = "GA",
+                        PostalCode = "12345"
+                    });
+
+            modelBuilder.Entity<Customer>()
+                .HasData(new
+                {
+                    Id = 1,
+                    CompanyName = "Smith Towing",
+                    AddressId = 1,
+                    Contact = "Jim",
+                    PhoneNumer = "555-1212"
+                },
+                new
+                {
+                    Id = 2,
+                    CompanyName = "Paintorama",
+                    AddressId = 2,
+                    Contact = "Phyllis",
+                    PhoneNumber = "555-2121"
+                });
+
+            modelBuilder.Entity<Employee>()
+                .HasData(new Employee()
+                {
+                    Id = 1,
+                    Name = "Mary Jones",
+                    BillingRate = 220f,
+                    ImageUrl = "/img/mary.jpg",
+                    Email = "mary@freebilling.com"
+                },
+                new Employee()
+                {
+                    Id = 2,
+                    Name = "Betty Patel",
+                    BillingRate = 85f,
+                    ImageUrl = "/img/betty.jpg",
+                    Email = "betty@freebilling.com"
+                },
+                new Employee()
+                {
+                    Id = 3,
+                    Name = "Nancy Smith",
+                    BillingRate = 115f,
+                    ImageUrl = "/img/nancy.jpg",
+                    Email = "nancy@freebilling.com"
+                },
+                new Employee()
+                {
+                    Id = 4,
+                    Name = "John Phillips",
+                    BillingRate = 145f,
+                    ImageUrl = "/img/john.jpg",
+                    Email = "john@freebilling.com"
+                });
+        }
     }
 }
