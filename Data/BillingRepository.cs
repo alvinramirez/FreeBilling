832d0f8fe30b03cf9d7601e07ea1cf8074b2f747
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-19 23:44:45 +0200

    Added a implementation interface for the BillingRepository

--- Data/BillingRepository.cs
+++ Data/BillingRepository.cs
@@ -1,34 +1,33 @@
 ﻿using FreeBilling.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace FreeBilling.Web.Data
+namespace FreeBilling.Web.Data;
+
+public class BillingRepository : IBillingRepository
 {
-    public class BillingRepository
+    private readonly BillingContext context;
+
+    public BillingRepository(BillingContext context)
     {
-        private readonly BillingContext context;
+        this.context = context;
+    }
 
-        public BillingRepository(BillingContext context)
-        {
-            this.context = context;
-        }
+    public async Task<IEnumerable<Employee>> GetEmployees()
+    {
+        return await this.context.Employees
+            .OrderBy(e => e.Name)
+            .ToListAsync();
+    }
 
-        public async Task<IEnumerable<Employee>> GetEmployees()
-        {
-            return await this.context.Employees
-                .OrderBy(e => e.Name)
-                .ToListAsync();
-        }
-        
-        public async Task<IEnumerable<Customer>> GetCustomers()
-        {
-            return await this.context.Customers
-                .OrderBy(c => c.CompanyName)
-                .ToListAsync();
-        }
+    public async Task<IEnumerable<Customer>> GetCustomers()
+    {
+        return await this.context.Customers
+            .OrderBy(c => c.CompanyName)
+            .ToListAsync();
+    }
 
-        public async Task<bool> SaveChanges()
-        {
-            return await context.SaveChangesAsync() > 0;
-        }
+    public async Task<bool> SaveChanges()
+    {
+        return await context.SaveChangesAsync() > 0;
     }
 }
