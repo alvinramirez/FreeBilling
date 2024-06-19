625238a07198a363b10180086921439c49c6895b
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-19 23:31:12 +0200

    Created a new file interface for the BillingRepository create a mock version in testing

---
+++ Data/IBillingRepository.cs
@@ -0,0 +1,10 @@
+﻿using FreeBilling.Data.Entities;
+
+namespace FreeBilling.Web.Data;
+
+public interface IBillingRepository
+{
+    Task<IEnumerable<Customer>> GetCustomers();
+    Task<IEnumerable<Employee>> GetEmployees();
+    Task<bool> SaveChanges();
+}
