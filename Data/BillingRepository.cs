d65902d7ee62bc4af45dccc0dc801d088bec26f2
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-20 23:09:59 +0200

    Added ILogger interface for event errors register

--- Data/BillingRepository.cs
+++ Data/BillingRepository.cs
@@ -6,17 +6,27 @@ namespace FreeBilling.Web.Data;
 public class BillingRepository : IBillingRepository
 {
     private readonly BillingContext context;
+    private readonly ILogger<BillingRepository> logger;
 
-    public BillingRepository(BillingContext context)
+    public BillingRepository(BillingContext context, ILogger<BillingRepository> logger)
     {
         this.context = context;
+        this.logger = logger;
     }
 
     public async Task<IEnumerable<Employee>> GetEmployees()
     {
-        return await this.context.Employees
-            .OrderBy(e => e.Name)
-            .ToListAsync();
+        try
+        {
+            return await this.context.Employees
+                .OrderBy(e => e.Name)
+                .ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Could not get employees: {ex.Message}");
+            throw;
+        }
     }
 
     public async Task<IEnumerable<Customer>> GetCustomers()
