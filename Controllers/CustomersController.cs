6e328718ee4dc33cbf5687e41f462b9fbdd6a8a5
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-23 23:58:18 +0200

    Created a new controllers directory to manage the http verbs for the api

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using FreeBilling.Data.Entities;
+using FreeBilling.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeBilling.Web.Controllers
+{
+    public class CustomersController : ControllerBase
+    {
+        private readonly IBillingRepository repository;
+
+        public CustomersController(IBillingRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<IEnumerable<Customer>> Get()
+        {
+            return await repository.GetCustomers();
+        }
+    }
+}
