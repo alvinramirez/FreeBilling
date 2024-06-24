e330fc92042f147f81ebef71faeace85fc4dccfe
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-24 22:46:23 +0200

    Added an asynchronous task passing it an id as a parameter to obtain a specific record

--- Data/IBillingRepository.cs
+++ Data/IBillingRepository.cs
@@ -5,6 +5,8 @@ namespace FreeBilling.Web.Data;
 public interface IBillingRepository
 {
     Task<IEnumerable<Customer>> GetCustomers();
+    Task<Customer?> GetCustomer(int id);
+
     Task<IEnumerable<Employee>> GetEmployees();
     Task<bool> SaveChanges();
 }
