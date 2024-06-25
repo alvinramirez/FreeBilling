50f9eb7b40c950cbcdbe20bdb548f2d8ce12da20
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-06-25 23:16:33 +0200

    Added a new implementation of the IBillingRepository for the method GetCustomersWithAddresses

--- Data/IBillingRepository.cs
+++ Data/IBillingRepository.cs
@@ -5,6 +5,7 @@ namespace FreeBilling.Web.Data;
 public interface IBillingRepository
 {
     Task<IEnumerable<Customer>> GetCustomers();
+    Task<IEnumerable<Customer>> GetCustomersWithAddresses();
     Task<Customer?> GetCustomer(int id);
 
     Task<IEnumerable<Employee>> GetEmployees();
