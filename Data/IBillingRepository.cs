b7ce8e52789fc60d0231dd65ce8a9d99f22f1b57
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-07-01 23:58:39 +0200

    Added a generic method on the interface contract

--- Data/IBillingRepository.cs
+++ Data/IBillingRepository.cs
@@ -9,5 +9,6 @@ public interface IBillingRepository
     Task<Customer?> GetCustomer(int id);
     Task <TimeBill?> GetTimeBill(int id);
     Task<IEnumerable<Employee>> GetEmployees();
+    void AddEntity<T>(T entity) where T : notnull;
     Task<bool> SaveChanges();
 }
