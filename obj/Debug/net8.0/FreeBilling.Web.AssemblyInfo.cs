cb51b8fb31f4fc856473d1da9c4849df5e4a19ac
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-07-30 00:28:50 +0200

    Modified files automatically generated by the compilation process

--- obj/Debug/net8.0/FreeBilling.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/FreeBilling.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreeBilling.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da58b6f4302411da1541b08b988f9d4a2cc63a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+398df00366a71c8a836ffa450ed44931024df035")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreeBilling.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreeBilling.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
