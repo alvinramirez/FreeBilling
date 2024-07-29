421c0b99035ffc906a92da97a80021d032c74ee0
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-07-30 00:28:40 +0200

    Added temporarily the employeeid and the customerid to see it and work with it

--- Data/Entities/TimeBill.cs
+++ Data/Entities/TimeBill.cs
@@ -5,7 +5,9 @@ namespace FreeBilling.Data.Entities;
 public class TimeBill
 {
   public int Id { get; set; }
+  public int? EmployeeId { get; set; }
   public Employee? Employee { get; set; }
+  public int? CustomerId { get; set; }
   public Customer? Customer { get; set; }
   public double Hours { get; set; }
   public double BillingRate { get; set; }
