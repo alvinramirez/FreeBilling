917a79b302ea60a22318446448c041df778eed05
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-08-15 23:58:31 +0200

    Created new directory for using models

---
+++ Models/TimeBillModel.cs
@@ -0,0 +1,15 @@
+﻿using FreeBilling.Data.Entities;
+
+namespace FreeBilling.Web.Models
+{
+    public class TimeBillModel
+    {
+        public int Id { get; set; }
+        public int? EmployeeId { get; set; }
+        public int? CustomerId { get; set; }
+        public double Hours { get; set; }
+        public double BillingRate { get; set; }
+        public DateTime? Date { get; set; }
+        public string? WorkPerformed { get; set; }
+    }
+}
