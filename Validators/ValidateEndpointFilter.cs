b9706c932e20977852e6e0ffec0c6ac7eec80655
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-08-18 23:18:16 +0200

    Added an endpoint filter to automatically integrate model validation before the endpoint logic executed returning a detailed error if the validation fail

---
+++ Validators/ValidateEndpointFilter.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FreeBilling.Web.Validators
+{
+    public class ValidateEndpointFilter<T> : IEndpointFilter
+        where T : class
+    {
+        public ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
