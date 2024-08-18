3f606e12b6a906e319e284c153ef708c11dde846
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-08-18 23:09:54 +0200

    Added model validation for TimeBillModel using FluentValidation in the POST endpoint that ensures data validity enhancing API robustness and reliability

--- Apis/TimeBillsApi.cs
+++ Apis/TimeBillsApi.cs
@@ -1,6 +1,8 @@
-﻿using FreeBilling.Data.Entities;
+﻿using FluentValidation;
+using FreeBilling.Data.Entities;
 using FreeBilling.Web.Data;
 using FreeBilling.Web.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace FreeBilling.Web.Apis
 {
@@ -13,7 +15,26 @@ namespace FreeBilling.Web.Apis
             group.MapGet("{id:int}", GetTimeBill)
                 .WithName("GetTimeBill");
 
-            group.MapPost("", PostTimeBill);
+            group.MapPost("", PostTimeBill)
+                .AddEndpointFilter(async (context, next) =>
+                {
+                    var validator = context.HttpContext.RequestServices.GetRequiredService<IValidator<TimeBillModel>>();
+
+                    var model = context.Arguments
+                    .OfType<TimeBillModel>()
+                    .FirstOrDefault();
+
+                    if (model is not null)
+                    {
+                        var validation = validator.Validate(model);
+
+                        if (!validation.IsValid)
+                        {
+                            return Results.ValidationProblem(validation.ToDictionary());
+                        }
+                    }
+                    return await next(context);
+                });
         }
 
         public static async Task<IResult> GetTimeBill(IBillingRepository repository, 
@@ -26,7 +47,8 @@ namespace FreeBilling.Web.Apis
             return Results.Ok(bill);
         }
 
-        public static async Task<IResult> PostTimeBill(IBillingRepository repository, TimeBillModel model)
+        public static async Task<IResult> PostTimeBill(IBillingRepository repository,
+            TimeBillModel model)
         {
             var newEntity = new TimeBill()
             {
