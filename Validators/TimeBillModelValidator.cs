592a6c589d1114600f1434f51e806a43f42a38f9
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-08-18 23:12:57 +0200

    Added validation rules for TimeBillModel to ensure data intregrity

--- Validators/TimeBillModelValidator.cs
+++ Validators/TimeBillModelValidator.cs
@@ -1,6 +1,24 @@
-﻿namespace FreeBilling.Web.Validators
+﻿using FluentValidation;
+using FreeBilling.Web.Models;
+
+namespace FreeBilling.Web.Validators
 {
-    public class TimeBillModelValidator
+    public class TimeBillModelValidator : AbstractValidator<TimeBillModel>
     {
+        public TimeBillModelValidator()
+        {
+            RuleFor(m => m.CustomerId).NotEmpty();
+            RuleFor(m => m.EmployeeId).NotEmpty();
+
+            RuleFor(m => m.Rate)
+                .InclusiveBetween(50.0, 300.0);
+
+            RuleFor(m => m.HoursWorked)
+                .InclusiveBetween(.1, 12.0);
+
+            RuleFor(m => m.Work)
+                .NotEmpty()
+                .MinimumLength(15);
+        }
     }
 }
