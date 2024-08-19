37feb47fb70b748d790dabfd9b922d460775c155
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-08-19 23:57:03 +0200

    Added implementation function which validates a model using FluentValidation and handles the request flow based on the result of that validation

--- Validators/ValidateEndpointFilter.cs
+++ Validators/ValidateEndpointFilter.cs
@@ -1,11 +1,33 @@
 ﻿
+using FluentValidation;
+using FreeBilling.Web.Models;
+
 namespace FreeBilling.Web.Validators
 {
     public class ValidateEndpointFilter<T> : IEndpointFilter
         where T : class
     {
-        public ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, 
+            EndpointFilterDelegate next)
         {
+            var validator = context.HttpContext
+                .RequestServices.GetRequiredService<IValidator<T>>();
+
+            var model = context.Arguments
+            .OfType<T>()
+            .FirstOrDefault();
+
+            if (model is not null)
+            {
+                var validation = validator.Validate(model);
+
+                if (!validation.IsValid)
+                {
+                    return Results.ValidationProblem(validation.ToDictionary());
+                }
+            }
+            return await next(context);
+
             throw new NotImplementedException();
         }
     }
