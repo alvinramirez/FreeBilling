a167a83aec99696953524beb96ba9e22077406e4
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-08-20 16:27:00 +0200

    Created a configuration mapping between TimeBillModel and TimeBill using Mapster defining how properties should be translated from one to another since the mapping is bidirectional

---
+++ Data/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using FreeBilling.Data.Entities;
+using FreeBilling.Web.Models;
+using Mapster;
+
+namespace FreeBilling.Web.Data
+{
+    public class MappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<TimeBillModel, TimeBill>()
+                .TwoWays()
+                .Map(d => d.BillingRate, s => s.Rate)
+                .Map(d => d.WorkPerformed, s => s.Work)
+                .Map(d => d.Hours, s => s.HoursWorked);
+        }
+    }
+}
