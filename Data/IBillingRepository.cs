628a424918e8fb1ea419389a6df03ccbf86b7fbe
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-08-22 23:33:49 +0200

    Added the two new methods to the this interface implementation

--- Data/IBillingRepository.cs
+++ Data/IBillingRepository.cs
@@ -8,6 +8,8 @@ public interface IBillingRepository
     Task<IEnumerable<Customer>> GetCustomersWithAddresses();
     Task<Customer?> GetCustomer(int id);
     Task <TimeBill?> GetTimeBill(int id);
+    Task<IEnumerable<TimeBill>> GetTimeBillsForCustomer(int id);
+    Task <TimeBill?> GetTimeBillForCustomer(int id, int billId);
     Task<IEnumerable<Employee>> GetEmployees();
     void AddEntity<T>(T entity) where T : notnull;
     Task<bool> SaveChanges();
