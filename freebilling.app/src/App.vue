f3d2d20812d42370137f99ef0b0ca01ad5a3f5ba
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-09-19 15:37:00 +0200

    Deleted some tags such as styles, div, img and calling of the generated vue components

--- freebilling.app/src/App.vue
+++ freebilling.app/src/App.vue
@@ -3,43 +3,10 @@
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+      <h3>Our App</h3>
   </header>
 
   <main>
-    <TheWelcome />
+      <h1>Hello from Vue</h1>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
