4c3aa3211d6c82a3a591cf47336e6ca615257352
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-10-03 22:54:57 +0200

    Modified some tags code creating js objects and adding an image for testing vue functionality

--- freebilling.app/src/App.vue
+++ freebilling.app/src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
-    let name = "Alvin";
+    import { ref } from "vue";
+
+    const name = ref("Alvin");
+
+    const nancy = ref("Nancy Smith");
+
     function changeMe()
     {
-        name = "Ivis";
-        alert(name);
+        name.value += "+";
+        //alert(name);
     }
 </script>
 
 <template>
-  <header class="flex text-red-900">
+  <header class="flex" style="color: #900;">
       <h3>Our App</h3>
   </header>
 
@@ -16,5 +21,6 @@
       <h1>Hello from Vue</h1>
       <div>{{ name }}</div>
       <button class="btn" @click="changeMe">Change Me</button>
+      <img src="/nancy.jpg" :alt="nancy" :title="nancy"/>
   </main>
 </template>
