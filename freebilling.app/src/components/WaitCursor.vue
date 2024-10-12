1977da6cc556a5851e8b6253e2a21239bc90a3b8
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-10-12 23:51:43 +0200

    Created new directory for vue components added a wait cursor components for see more details of vue

---
+++ freebilling.app/src/components/WaitCursor.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+    defineProps({
+        "busy": { required: true, type: Boolean },
+        "msg": { type: String, default: "Loading..." }
+    });
+
+</script>
+
+<template>
+    <div v-if="busy" class="bg-blue-900 text-white p-2 text-lg">{{ msg }}</div>
+</template>
