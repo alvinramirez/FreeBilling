8a2d4b9ecc589e9989d80b10c6c67b18da51b16b
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-10-15 23:55:07 +0200

    Added new page to the template tag to see multiple pages

--- freebilling.app/src/App.vue
+++ freebilling.app/src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <ul class="text-blue-900 font-bold flex">
-        <li><RouterLink to="/"></RouterLink></li>
+        <li><RouterLink :to="{ name: 'Home' }">Home</RouterLink></li>
+        <li><RouterLink :to="{ name: 'Billing' }">Billing</RouterLink></li>
     </ul>
 
     <div class="bg-gray-300 p-2">
