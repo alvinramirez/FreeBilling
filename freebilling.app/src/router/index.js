d9e5a63a55e85c361bb7200b6bf8d629162456c4
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-10-15 23:53:09 +0200

    Added new route to the router and changing import directory from

--- freebilling.app/src/router/index.js
+++ freebilling.app/src/router/index.js
@@ -1,12 +1,19 @@
 // router
-import HomeView from "../views/HomeView.vue"
+import HomeView from "@/views/HomeView.vue";
+import BillingView from "@/views/BillingView.vue";
 import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 
 const routes = [
     {
+        name: "Home",
         path: "/",
         component: HomeView
+    },
+    {
+        path: "/billing",
+        component: BillingView
     }
+
 ];
 
 const router = createRouter({
