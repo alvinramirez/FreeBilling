8ed69458a8f12ba1864c308ccdb9886942774ef6
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-10-21 19:24:15 +0200

    Created new view for enabling authentication login

---
+++ freebilling.app/src/views/LoginView.vue
@@ -0,0 +1,53 @@
+<script setup>
+    import { ref } from "vue";
+    import axios from "axios";
+
+    const username = ref("");
+    const password = ref("");
+    const message = ref("");
+
+    async function login() {
+        try {
+            const result = await axios.post("/api/auth/token", {
+                username: username.value,
+                password: password.value
+            })
+        } catch {
+            message.value = "Wrong username or password";
+        }
+    }
+</script>
+
+<template>
+    <div class="w-96 mx-auto p-6 bg-white shadow-md rounded">
+        <h3 class="text-2x1 mb-4 text-center">Login</h3>
+        <form novalidate @submit="login">
+
+        <div v-if="message" class="mb-4 text-red-500 text-center">{{ message }}</div>
+
+        <div class="mb-4">
+            <label for="username" class="block mb-1 font-semibold">Username</label>
+            <input type="text"
+                   id="username"
+                   v-model="username"
+                   class="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                   placeholder="Username" />
+        </div>
+
+        <div class="mb-6">
+            <label for="password" class="block mb-1 font-semibold">Password</label>
+            <input type="password"
+                   id="password"
+                   v-model="password"
+                   class="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                   placeholder="Password" />
+        </div>
+
+        <button type="submit"
+                @click="login"
+                class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-1000">
+                Login
+        </button>
+        </form>
+    </div>
+</template>
