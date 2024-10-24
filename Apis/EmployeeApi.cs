3306aa1beaa3f57b78fc3788c51f99ccf05afd32
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-10-24 23:55:35 +0200

    Created new minimal api for serving employees to use it in vue app

---
+++ Apis/EmployeeApi.cs
@@ -0,0 +1,18 @@
+﻿using FreeBilling.Web.Data;
+
+namespace FreeBilling.Web.Apis
+{
+    public static class EmployeeApis
+    {
+        public static void Register(WebApplication app)
+        {
+            app.MapGet("/api/employees", GetEmployees)
+            .RequireAuthorization("ApiPolicy");
+        }
+
+        public static async Task<IResult> GetEmployees(IBillingRepository repository)
+        {
+            return Results.Ok(await repository.GetEmployees());
+        }
+    }
+}
