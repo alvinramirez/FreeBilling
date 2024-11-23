38784376654c47a56a7561c63511b45bcfe0aaf3
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-11-23 23:47:51 +0100

    Created new minimal api for users authentication

---
+++ Apis/AuthApi.cs
@@ -0,0 +1,18 @@
+﻿using FreeBilling.Web.Data;
+
+namespace FreeBilling.Web.Apis
+{
+    public static class AuthApi
+    {
+        public static void Register(WebApplication app)
+        {
+            app.MapGet("/api/auth", GetAuth)
+            .RequireAuthorization("ApiPolicy");
+        }
+
+        public static async Task<IResult> GetAuth(IBillingRepository repository)
+        {
+            return Results.Ok(await repository.GetAuth());
+        }
+    }
+}
