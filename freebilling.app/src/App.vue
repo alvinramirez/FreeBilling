04c0bcef0d7c7be1b6c0750c4761d1a4454fdef7
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-12-25 23:59:05 +0100

    Added new link tag for logout and validate if there is a token also added a style for entire unordered list

--- freebilling.app/src/App.vue
+++ freebilling.app/src/App.vue
@@ -10,10 +10,11 @@
 </script>
 
 <template>
-    <ul class="text-blue-900 font-bold flex justify-end">
+    <ul class="text-blue-900 font-bold flex justify-end mr-2">
         <li><RouterLink :to="{ name: 'Home' }">Home</RouterLink></li>
         <li><RouterLink :to="{ name: 'Billing' }">Billing</RouterLink></li>
         <li><RouterLink v-if="!state.token" :to="{ name: 'Login' }">Login</RouterLink></li>
+        <li><a href="" v-if="state.token" @click="logout">Logout</a></li>
     </ul>
 
     <div class="bg-gray-300 p-2">
