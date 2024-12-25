0c2ad8a1d1682700bfde3684305c5c0bb8c8a179
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-12-25 23:57:02 +0100

    Added validation if there isn't a token show the login

--- freebilling.app/src/App.vue
+++ freebilling.app/src/App.vue
@@ -11,9 +11,9 @@
 
 <template>
     <ul class="text-blue-900 font-bold flex justify-end">
-        <li class="mr-2"><RouterLink :to="{ name: 'Home' }">Home</RouterLink></li>
+        <li><RouterLink :to="{ name: 'Home' }">Home</RouterLink></li>
         <li><RouterLink :to="{ name: 'Billing' }">Billing</RouterLink></li>
-        <li><RouterLink :to="{ name: 'Login' }">Login</RouterLink></li>
+        <li><RouterLink v-if="!state.token" :to="{ name: 'Login' }">Login</RouterLink></li>
     </ul>
 
     <div class="bg-gray-300 p-2">
