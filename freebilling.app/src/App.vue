9f21b58f5c9d438e925eb81f58c0ea47576d8d43
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-12-25 23:52:08 +0100

    Added new router link tag for login and script setup for logout

--- freebilling.app/src/App.vue
+++ freebilling.app/src/App.vue
@@ -1,7 +1,19 @@
+<script setup>
+    import state from "@/state";
+    import { useRouter } from "vue-router";
+    import router from "./router/index";
+
+    function logout() {
+        state.token = "";
+        router.push("/");
+    }
+</script>
+
 <template>
     <ul class="text-blue-900 font-bold flex justify-end">
         <li class="mr-2"><RouterLink :to="{ name: 'Home' }">Home</RouterLink></li>
         <li><RouterLink :to="{ name: 'Billing' }">Billing</RouterLink></li>
+        <li><RouterLink :to="{ name: 'Login' }">Login</RouterLink></li>
     </ul>
 
     <div class="bg-gray-300 p-2">
