6f118b68e813fa6933207afd95d9600e694da8f2
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-12-28 14:14:32 +0100

    Added loadCustomers method to state for handling customer data retrieval via API

--- freebilling.app/src/state/index.js
+++ freebilling.app/src/state/index.js
@@ -1,10 +1,20 @@
 import { reactive } from "vue";
-
+import axios from "axios";
 
 export default reactive({
     token: "",
     customers: [],
     employees: [],
     timeBills: [],
-    currentCustomer: null
-})
+    currentCustomer: null,
+    async loadCustomers() {
+        if (this.customers.length == 0) {
+            const customerResult = await axios.get("/api/customers", {
+                headers: {
+                    "Authorization": `Bearer ${this.token}`
+                }
+            });
+            this.customers.splice(this.customers, this.customers.length, ...customerResult.data);
+        }
+    }
+});
