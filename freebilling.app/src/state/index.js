75e1e370b8533d4a7c35941703cc02780b581802
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-12-30 18:08:40 +0100

    Added loadEmployees method to state for handling customer data retrieval via API

--- freebilling.app/src/state/index.js
+++ freebilling.app/src/state/index.js
@@ -8,13 +8,23 @@ export default reactive({
     timeBills: [],
     currentCustomer: null,
     async loadCustomers() {
-        if (this.customers.length == 0) {
+        if (this.customers.length === 0) {
             const customerResult = await axios.get("/api/customers", {
                 headers: {
                     "Authorization": `Bearer ${this.token}`
                 }
             });
             this.customers.splice(this.customers, this.customers.length, ...customerResult.data);
+        }
+    },
+    async loadEmployees() {
+        if (this.employees.length === 0) {
+            const employeesResult = await axios.get("/api/employees", {
+                headers: {
+                    Authorization: `Bearer ${state.token}`
+                }
+            });
+            this.employees.splice(this.employees, this.employees.length, ...employeesResult.data);
         }
     }
 });
