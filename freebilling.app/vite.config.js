cba7c400d1f7dcca7038ba05e36badcd7d1a46a4
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-01-04 19:31:09 +0100

    Added build.outDir setting to change the output directory of built files for allow the frontend project to be integrated with the existing backend server

--- freebilling.app/vite.config.js
+++ freebilling.app/vite.config.js
@@ -7,7 +7,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-  ],
+    ],
+    build: {
+        outDir: "../FreeBilling.Web/wwroot/app/"
+    },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
