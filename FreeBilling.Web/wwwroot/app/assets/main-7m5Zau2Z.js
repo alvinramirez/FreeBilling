3add9bdc59b700756fcc288074c63c8b88d99d93
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-01-08 23:33:02 +0100

    Added base template to initialize Vue-based admin interface with routing and global styles

---
+++ FreeBilling.Web/wwwroot/app/assets/main-7m5Zau2Z.js
@@ -0,0 +1,1 @@
+import{c as a,r,_ as p}from"./App-B-xFBuoG.js";a(p).use(r).mount("#app");
---
+++ freebilling.app/src/admin.js
@@ -0,0 +1,9 @@
+import '../../wwwroot/site.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router';
+
+createApp(App)
+    .use(router)
+    .mount('#app')
